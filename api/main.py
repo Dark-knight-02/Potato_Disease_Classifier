0aa197931ca1a97a28cc999c97e02cadd58898b7
Dark-knight-02 <pranav.s.cd.mat20@itbhu.ac.in>
2022-01-10 00:07:28 +0530

    first commit

---
+++ api/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn 
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../saved_models/1")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+@app.get("/ping")
+
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image  = np.array(Image.open(BytesIO(data)))
+    return image
+
+# get the file
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0) # adds one more dimension [255, 255, 3] -> [[255, 255, 3]]
+    # now our input is ready for the prediction
+    prediction = MODEL.predict(img_batch)
+    index = np.argmax(prediction[0])
+    predicted_class = CLASS_NAMES[index]
+    confidence = np.max(prediction[0])
+
+    return {
+        'class':predicted_class,
+        'confidence':float(confidence)
+    }
+
+if __name__=="__main__":
+    uvicorn.run(app, host='localhost', port=8000)
